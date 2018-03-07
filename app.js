71e06517ed3f9c3424fcc58469f7e7c47e1ff366
Kaykay Adewunmi <kay.d.granz@outlook.com>
2018-03-07 05:00:18 +0000

    Learn and code CSS Grid project

---
+++ app.js
@@ -0,0 +1,19 @@
+
+var express = require("express");
+var app     = express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+
+
+app.get("/", function(req, res){
+   res.render("nexter"); 
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(req, res){
+   console.log("Nexter server is reset and you can now launch the Nexter application!!"); 
+});
